e07d29de70e4d599336f2e9d48ee3bba7bb6a6cf
Andrii Antoniuk <aantoniuk@basquare.com>
2019-07-05 10:41:06 +0300

    update H&C

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const cron = require('node-cron');
 const SimpleNodeLogger = require('simple-node-logger');
 
-//const { transporter, mailOptionStarted, mailOptionFinished } = require('./emailSender');
+const { sgMail, msgStart, msgFinish } = require("./emailSender");
 const { updateCustomers, updateOrderHdrs, updateOrderDtls, today } = require('./functions');
 
-cron.schedule('0 15 8 * * *', async () => {
+cron.schedule('0 40 10 * * *', async () => {
     const log = SimpleNodeLogger.createSimpleLogger({
         logFilePath: `logs/${today()}.log`,
         timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
     });
     
-    log.info(today() + ' CRON JOB STARTED');
-    //transporter.sendEmail(mailOptionStarted);
-    log.warn(' ========== CUSTOMERS TABLE ========== ');
+    log.info(today() + " CRON JOB STARTED");
+    sgMail.send(msgStart);
+    log.warn(" ========== CUSTOMERS TABLE ========== ");
     await updateCustomers(log);
-    log.warn(' ========== ORDER HEADERS TABLE ========== ');
+    log.warn(" ========== ORDER HEADERS TABLE ========== ");
     await updateOrderHdrs(log);
-    log.warn(' ========== ORDER DETAILS TABLE ========== ');
+    log.warn(" ========== ORDER DETAILS TABLE ========== ");
     await updateOrderDtls(log);
-    //transporter.sendEmail(mailOptionFinished);
-    log.info('CRON JOB ENDED');
+    sgMail.send(msgFinish);
+    log.info("CRON JOB ENDED");
 });
